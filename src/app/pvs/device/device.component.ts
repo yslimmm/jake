47d3447c67133e3b55c5311b89f3251b170d9d07
yslim <yslim93@naver.com>
2018-08-16 18:33:58 +0900

    - DB 연동부분 우선 주석 처리. - 기본 단말 정보 select 추가.

--- src/app/pvs/device/device.component.ts
+++ src/app/pvs/device/device.component.ts
@@ -10,6 +10,19 @@ import {HttpClient} from "@angular/common/http";
 import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
 import {MatSnackBar} from "@angular/material";
 
+
+export interface DeviceList {
+  value: string;
+  modelValue: string;
+  viewValue: string;
+}
+
+export interface DeviceGroup {
+  disabled?: boolean;           // optional
+  name: string;
+  deviceList: DeviceList[];
+}
+
 @Component({
   selector: 'app-device',
   templateUrl: './device.component.html',
@@ -21,7 +34,218 @@ export class DeviceComponent implements OnInit {
   dbName: String;
 
   selectDBOption: string;
+
+  // TODO : 전부 설정파일로 빼놓기!!!!
   dbOptions: string[] = ['사내', '개발1', '개발3', '개발4'];
+
+  hubs: string[] = [
+    '독립형허브(고급)', '보급형허브', '동글형허브1(구형)', '동글형허브2(신형)', 'LTE동글형허브', 'WOOFER', 'TVG', '홈넷 월패드허브'
+  ];
+
+  deviceControl = new FormControl();
+  pvsDeviceGroups: DeviceGroup[] = [
+    {
+      name: '전략단말(CGS) - ZW',
+      deviceList: [
+        {value: 'cgs_zw_01', modelValue: '', viewValue: '삼성도어락'},
+        {value: 'cgs_zw_02', modelValue: '',  viewValue: 'GM도어락(보조키)'},
+        {value: 'cgs_zw_03', modelValue: '',  viewValue: 'GM도어락(주키)'},
+        {value: 'cgs_zw_04', modelValue: '',  viewValue: '플러그1(신형/SPD-01A)'},
+        {value: 'cgs_zw_05', modelValue: '',  viewValue: '플러그1(개선형/SPD-02A)'},
+        {value: 'cgs_zw_06', modelValue: '',  viewValue: '플러그1(구형/SPD-01)'},
+        {value: 'cgs_zw_07', modelValue: '',  viewValue: '플러그2(이지세이버/SPES-02)'},
+        {value: 'cgs_zw_08', modelValue: '',  viewValue: '플러그2(개선형/SPES-02A)'},
+        {value: 'cgs_zw_09', modelValue: '',  viewValue: '에너지미터'},
+        {value: 'cgs_zw_10', modelValue: '',  viewValue: '가스락'},
+        {value: 'cgs_zw_11', modelValue: '',  viewValue: '열림센서'},
+        {value: 'cgs_zw_12', modelValue: '',  viewValue: '열림감지센서(LG이노텍)'},
+        {value: 'cgs_zw_13', modelValue: '',  viewValue: '스위치47(구형)'},
+        {value: 'cgs_zw_14', modelValue: '',  viewValue: '스위치48(구형)'},
+        {value: 'cgs_zw_15', modelValue: '',  viewValue: '스위치49(구형)'},
+        {value: 'cgs_zw_16', modelValue: '',  viewValue: '스위치4A(신형)'},
+        {value: 'cgs_zw_17', modelValue: '',  viewValue: '스위치4B(신형)'},
+        {value: 'cgs_zw_18', modelValue: '',  viewValue: '스위치4C(신형)'},
+        {value: 'cgs_zw_19', modelValue: '',  viewValue: '스위치4A(개선형)'},
+        {value: 'cgs_zw_20', modelValue: '',  viewValue: '스위치4B(개선형)'},
+        {value: 'cgs_zw_21', modelValue: '',  viewValue: '스위치4C(개선형)'},
+        {value: 'cgs_zw_22', modelValue: '',  viewValue: '대용량스위치 1구'},
+        {value: 'cgs_zw_23', modelValue: '',  viewValue: '대용량스위치 2구'},
+        {value: 'cgs_zw_24', modelValue: '',  viewValue: '대용량스위치 3구'},
+        {value: 'cgs_zw_25', modelValue: '',  viewValue: '주방소화장치(파라텍)'},
+        {value: 'cgs_zw_26', modelValue: '',  viewValue: '도어캠'},
+        {value: 'cgs_zw_27', modelValue: '',  viewValue: '무선경보기(사이렌)'},
+        {value: 'cgs_zw_28', modelValue: '',  viewValue: '솜피커튼'},
+        {value: 'cgs_zw_29', modelValue: '',  viewValue: '솜피블라인드'},
+        {value: 'cgs_zw_30', modelValue: '',  viewValue: '모션센서'},
+        {value: 'cgs_zw_31', modelValue: '',  viewValue: '움직임알리미'},
+        {value: 'cgs_zw_32', modelValue: '',  viewValue: 'IoT버튼'}
+      ]
+    },
+    {
+      name: '전략단말(CGS) - ZIGBEE',
+      deviceList: [
+        {value: 'cgs_zigBee_01', modelValue: '',  viewValue: '스마트 전등스위치(1접점)'},
+        {value: 'cgs_zigBee_02', modelValue: '',  viewValue: '스마트 전등스위치(2접점)'},
+        {value: 'cgs_zigBee_03', modelValue: '',  viewValue: '스마트 전등스위치(3접점)'},
+        {value: 'cgs_zigBee_04', modelValue: '',  viewValue: '스마트 콘센트(2구)'}
+      ]
+    },
+    {
+      name: '청약제휴단말(CGS) - WIFI',
+      // disabled: true,
+      deviceList: [
+        {value: 'cgs_wifi_01', modelValue: '',  viewValue: '펫스테이션'}
+      ]
+    },
+    {
+      name: '청약제휴단말(OGS) - WIFI',
+      // disabled: true,
+      deviceList: [
+        {value: 'ogs_wifi_01', modelValue: '',  viewValue: 'WIFI 플러그'},
+        {value: 'ogs_wifi_02', modelValue: '',  viewValue: 'WIFI 멀티탭'},
+        {value: 'ogs_wifi_03', modelValue: '',  viewValue: '멀티탭'},
+        {value: 'ogs_wifi_04', modelValue: '',  viewValue: '실내기'},
+        {value: 'ogs_wifi_05', modelValue: '',  viewValue: '보급형 실내기'},
+        {value: 'ogs_wifi_06', modelValue: '',  viewValue: '실외기'}
+      ]
+    }
+  ];
+
+  homenetDeviceGroups: DeviceGroup[] = [
+    {
+      name: '코콤',
+      deviceList: [
+        {value: 'homenet_kocom_01', modelValue: '', viewValue: '에어컨'},
+        {value: 'homenet_kocom_02', modelValue: '', viewValue: '온도조절기'},
+        {value: 'homenet_kocom_03', modelValue: '', viewValue: '가스밸브'},
+        {value: 'homenet_kocom_04', modelValue: '', viewValue: '관리비'},
+        {value: 'homenet_kocom_05', modelValue: '', viewValue: '방문자'},
+        {value: 'homenet_kocom_06', modelValue: '', viewValue: '주차관제'},
+        {value: 'homenet_kocom_07', modelValue: '', viewValue: '에너지'},
+        {value: 'homenet_kocom_08', modelValue: '', viewValue: '전열교환기'},
+        {value: 'homenet_kocom_09', modelValue: '', viewValue: '일괄소등스위치'},
+        {value: 'homenet_kocom_10', modelValue: '', viewValue: '지역정보'},
+        {value: 'homenet_kocom_11', modelValue: '', viewValue: '공지사항'},
+        {value: 'homenet_kocom_12', modelValue: '', viewValue: '무인택배'},
+        {value: 'homenet_kocom_13', modelValue: '', viewValue: '주차위치확인'},
+        {value: 'homenet_kocom_14', modelValue: '', viewValue: '주민투표'},
+        {value: 'homenet_kocom_15', modelValue: '', viewValue: '방범알림'},
+        {value: 'homenet_kocom_16', modelValue: '', viewValue: '대기전력차단'},
+        {value: 'homenet_kocom_17', modelValue: '', viewValue: '조명스위치'}
+      ]
+    },
+    {
+      name: '코콤동탄',
+      deviceList: [
+        {value: 'homenet_kocom_18', modelValue: '', viewValue: '에어컨'},
+        {value: 'homenet_kocom_19', modelValue: '', viewValue: '온도조절기'},
+        {value: 'homenet_kocom_20', modelValue: '', viewValue: '가스밸브'},
+        {value: 'homenet_kocom_21', modelValue: '', viewValue: '관리비'},
+        {value: 'homenet_kocom_22', modelValue: '', viewValue: '방문자'},
+        {value: 'homenet_kocom_23', modelValue: '', viewValue: '주차관제'},
+        {value: 'homenet_kocom_24', modelValue: '', viewValue: '에너지'},
+        {value: 'homenet_kocom_25', modelValue: '', viewValue: '전열교환기'},
+        {value: 'homenet_kocom_26', modelValue: '', viewValue: '일괄소등스위치'},
+        {value: 'homenet_kocom_27', modelValue: '', viewValue: '공지사항'},
+        {value: 'homenet_kocom_28', modelValue: '', viewValue: '무인택배'},
+        {value: 'homenet_kocom_29', modelValue: '', viewValue: '주차위치확인'},
+        {value: 'homenet_kocom_30', modelValue: '', viewValue: '주민투표'},
+        {value: 'homenet_kocom_31', modelValue: '', viewValue: '방범알림'},
+        {value: 'homenet_kocom_32', modelValue: '', viewValue: '대기전력차단'},
+        {value: 'homenet_kocom_33', modelValue: '', viewValue: '조명스위치'}
+      ]
+    },
+    {
+      name: '코맥스',
+      deviceList: [
+        {value: 'homenet_comax_01', modelValue: '', viewValue: '에어컨'},
+        {value: 'homenet_comax_02', modelValue: '', viewValue: '온도조절기'},
+        {value: 'homenet_comax_03', modelValue: '', viewValue: '가스밸브'},
+        {value: 'homenet_comax_04', modelValue: '', viewValue: '방문자'},
+        {value: 'homenet_comax_05', modelValue: '', viewValue: '에너지'},
+        {value: 'homenet_comax_06', modelValue: '', viewValue: '전열교환기'},
+        {value: 'homenet_comax_07', modelValue: '', viewValue: '일괄소등스위치'},
+        {value: 'homenet_comax_08', modelValue: '', viewValue: '지역정보'},
+        {value: 'homenet_comax_09', modelValue: '', viewValue: '공지사항'},
+        {value: 'homenet_comax_10', modelValue: '', viewValue: '무인택배'},
+        {value: 'homenet_comax_11', modelValue: '', viewValue: '주차위치확인'},
+        {value: 'homenet_comax_12', modelValue: '', viewValue: '주민투표'},
+        {value: 'homenet_comax_13', modelValue: '', viewValue: '방범알림'},
+        {value: 'homenet_comax_14', modelValue: '', viewValue: '대기전력차단'},
+        {value: 'homenet_comax_15', modelValue: '', viewValue: '조명스위치(조광)'},
+        {value: 'homenet_comax_16', modelValue: '', viewValue: '조명스위치'}
+      ]
+    },
+    {
+      name: '현대',
+      disabled: true,
+      deviceList: [
+        {value: 'homenet_hyundai_01', modelValue: '', viewValue: '에어컨'},
+        {value: 'homenet_hyundai_02', modelValue: '', viewValue: '온도조절기'},
+        {value: 'homenet_hyundai_03', modelValue: '', viewValue: '가스밸브'},
+        {value: 'homenet_hyundai_04', modelValue: '', viewValue: '관리비'},
+        {value: 'homenet_hyundai_05', modelValue: '', viewValue: '방문자'},
+        {value: 'homenet_hyundai_06', modelValue: '', viewValue: '주차관제'},
+        {value: 'homenet_hyundai_07', modelValue: '', viewValue: '에너지'},
+        {value: 'homenet_hyundai_08', modelValue: '', viewValue: '전열교환기'},
+        {value: 'homenet_hyundai_09', modelValue: '', viewValue: '일괄소등스위치'},
+        {value: 'homenet_hyundai_10', modelValue: '', viewValue: '지역정보'},
+        {value: 'homenet_hyundai_11', modelValue: '', viewValue: '공지사항'},
+        {value: 'homenet_hyundai_12', modelValue: '', viewValue: '무인택배'},
+        {value: 'homenet_hyundai_13', modelValue: '', viewValue: '주차위치확인'},
+        {value: 'homenet_hyundai_14', modelValue: '', viewValue: '주민투표'},
+        {value: 'homenet_hyundai_15', modelValue: '', viewValue: '방범알림'},
+        {value: 'homenet_hyundai_16', modelValue: '', viewValue: '대기전력차단'},
+        {value: 'homenet_hyundai_17', modelValue: '', viewValue: '조명스위치'}
+      ]
+    },
+    {
+      name: '이지빌',
+      disabled: true,
+      deviceList: [
+        {value: 'homenet_esay_01', modelValue: '', viewValue: '에어컨'},
+        {value: 'homenet_esay_02', modelValue: '', viewValue: '온도조절기'},
+        {value: 'homenet_esay_03', modelValue: '', viewValue: '가스밸브'},
+        {value: 'homenet_esay_04', modelValue: '', viewValue: '관리비'},
+        {value: 'homenet_esay_05', modelValue: '', viewValue: '방문자'},
+        {value: 'homenet_esay_06', modelValue: '', viewValue: '주차관제'},
+        {value: 'homenet_esay_07', modelValue: '', viewValue: '에너지'},
+        {value: 'homenet_esay_08', modelValue: '', viewValue: '전열교환기'},
+        {value: 'homenet_esay_09', modelValue: '', viewValue: '일괄소등스위치'},
+        {value: 'homenet_esay_10', modelValue: '', viewValue: '지역정보'},
+        {value: 'homenet_esay_11', modelValue: '', viewValue: '공지사항'},
+        {value: 'homenet_esay_12', modelValue: '', viewValue: '무인택배'},
+        {value: 'homenet_esay_13', modelValue: '', viewValue: '주차위치확인'},
+        {value: 'homenet_esay_14', modelValue: '', viewValue: '주민투표'},
+        {value: 'homenet_esay_15', modelValue: '', viewValue: '방범알림'},
+        {value: 'homenet_esay_16', modelValue: '', viewValue: '대기전력차단'},
+        {value: 'homenet_esay_17', modelValue: '', viewValue: '조명스위치'}
+      ]
+    },
+    {
+      name: 'CV-Net',
+      disabled: true,
+      deviceList: [
+        {value: 'homenet_cvnet_01', modelValue: '', viewValue: '에어컨'},
+        {value: 'homenet_cvnet_02', modelValue: '', viewValue: '온도조절기'},
+        {value: 'homenet_cvnet_03', modelValue: '', viewValue: '가스밸브'},
+        {value: 'homenet_cvnet_04', modelValue: '', viewValue: '관리비'},
+        {value: 'homenet_cvnet_05', modelValue: '', viewValue: '방문자'},
+        {value: 'homenet_cvnet_06', modelValue: '', viewValue: '주차관제'},
+        {value: 'homenet_cvnet_07', modelValue: '', viewValue: '에너지'},
+        {value: 'homenet_cvnet_08', modelValue: '', viewValue: '전열교환기'},
+        {value: 'homenet_cvnet_09', modelValue: '', viewValue: '일괄소등스위치'},
+        {value: 'homenet_cvnet_10', modelValue: '', viewValue: '지역정보'},
+        {value: 'homenet_cvnet_11', modelValue: '', viewValue: '공지사항'},
+        {value: 'homenet_cvnet_12', modelValue: '', viewValue: '무인택배'},
+        {value: 'homenet_cvnet_13', modelValue: '', viewValue: '주차위치확인'},
+        {value: 'homenet_cvnet_14', modelValue: '', viewValue: '주민투표'},
+        {value: 'homenet_cvnet_15', modelValue: '', viewValue: '방범알림'},
+        {value: 'homenet_cvnet_16', modelValue: '', viewValue: '대기전력차단'},
+        {value: 'homenet_cvnet_17', modelValue: '', viewValue: '조명스위치'}
+      ]
+    }
+  ];
 
   public form: FormGroup;
 
@@ -97,7 +321,8 @@ export class DeviceComponent implements OnInit {
         this.dbName =  "dev04";
         break;
     }
-    this.printChsDeviceModel(this.dbName);
+    // TODO : multi DB연동 테스트 성공하면 아래 주석 풀기
+    // this.printChsDeviceModel(this.dbName);
   }
 
   printChsDeviceModel(dbName: String): void {
@@ -218,38 +443,16 @@ export class DeviceComponent implements OnInit {
 
 
   // --------------------------------------------------- 워매.. 아까워라.... 공부용이니 냅두자...
-  // Radio DB Check
-  dbUrlCheck(): void {
-    // radio 버튼 선택된 값 가져오기
-    var radioVal = $('input[name="dbRadio"]:checked').val();
-
-    switch (radioVal) {
-      case "dev00" :  // 사내 DB
-        this.dbName =  "";
-        break;
-      case "dev01" :
-        this.dbName =  "";
-        break;
-      case "dev03" :
-        this.dbName =  "";
-        break;
-      case "dev04" :
-        this.dbName =  "";
-        break;
-    }
-    this.printChsDeviceModel(this.dbName);
-
-    /*
-     $.ajax({
-     type: 'post',
-     url: 'pvs/user.html',
-     dataType : 'html',
-     success: function(data) {
-     $("#pvsOptionBox").html(data);
-     }
-     });
-     */
-  };
+  /*
+   $.ajax({
+   type: 'post',
+   url: 'pvs/user.html',
+   dataType : 'html',
+   success: function(data) {
+   $("#pvsOptionBox").html(data);
+   }
+   });
+   */
 
   // table paged format.
   paginate(event) {
